584543ac5a8723aaf2fb5620b37ed5e78fbd49b8
shay xie <shailstorms@gmail.com>
2025-05-01 17:39:03 -0400

    track writings, books.json

---
+++ src/data/writings/index.tsx
@@ -0,0 +1,9 @@
+export interface Post {
+    slug: string;
+    title: string;
+}
+
+export const posts: Post[] = [
+    { slug: 'dreamt-thought', title: 'dreamt, thought.'},
+    { slug: 'hello-world', title: 'hello, world' },
+]
